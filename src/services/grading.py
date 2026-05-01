174e8477fc59eb58383a6168e9b975a7b486271d
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-05-01 20:36:41 +0300

    Add grading and components services

--- src/services/grading.py
+++ src/services/grading.py
@@ -1,7 +1,7 @@
 from src.models.components import Component
 
 
-def grading_2025() -> list[Component]:
+def components_2025() -> list[Component]:
     return [
         Component(name="Homework 0", max_points=1),
         Component(name="Homework 1", max_points=8),
@@ -9,3 +9,12 @@ def grading_2025() -> list[Component]:
         Component(name="Homework 3", max_points=8),
         Component(name="Project", max_points=30),
     ]
+
+
+def grading_2025(points: float) -> float:
+    if points < 30:
+        return 2.0
+    elif points > 60:
+        return 6.0
+    else:
+        return points / 10
