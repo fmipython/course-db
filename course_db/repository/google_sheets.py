43fdfd05fdebc7c9db7b551255c107dabaa01ff6
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-09-13 22:15:53 +0300

    Move to using service account

--- course_db/repository/google_sheets.py
+++ course_db/repository/google_sheets.py
@@ -1,8 +1,4 @@
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
@@ -10,8 +6,8 @@ from course_db.models.exceptions import GoogleSheetsError
 from course_db.services.big_table import BigTable
 from course_db.services.grading import components_2025, grading_2025
 
-# If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+SERVICE_ACCOUNT_FILE = "service_account.json"
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = "1NTZ26M6tKWzb5g7t0B83i4J-dQrqh7O-TnlAPxd4gsw"
@@ -31,25 +27,7 @@ def save(table: BigTable):
 
 
 def setup_auth():
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
-
-    return creds
+    return Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 
 def load_raw(creds, spreadsheet_id, spreadsheet_range) -> list[list[str]]:
